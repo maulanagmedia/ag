3577632ccf3303a55c9943b7e6c24be872741f86
gmediaiosdev2 <maulana.husni@gmedia.co.id>
2020-06-25 09:05:56 +0700

    maul

---
+++ library/build/generated/source/buildConfig/release/com/sothree/slidinguppanel/library/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sothree.slidinguppanel.library;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.sothree.slidinguppanel.library";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 17;
+  public static final String VERSION_NAME = "3.4.0";
+}
